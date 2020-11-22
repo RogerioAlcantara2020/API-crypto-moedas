eab92b6f95e6c874184ea79bb6b9dfbf5ea6f302
Rogerio Alcantara <carpe@carpetecnologia.com.br>
2020-11-22 17:14:50 -0300

    Edição Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Projeto Web utilizando a API Coin Market Cap
+
+ele consome as informações: name, symbol, first_historical_data
--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=$
             <img src='./img/coin.jpg' class='align-self-center mr-3' alt='Coin' width='100px' height='60px'>
             <div class='media-body'>
             <h5 class='mt-2'>Nome: ${api.data[i].name}</5>
-            <p>Simbolo: ${api.data[i].symbol}<br></p>
+            <p>Simbolo: ${api.data[i].symbol}<br>${api.data[i].first_historical_data}</p>
             </div>
         </div>
         `;
