6622d150b60d1747b667bd57c23695518782cc44
Rogerio Alcantara <carpe@carpetecnologia.com.br>
2020-11-22 17:04:23 -0300

    inserção da img da moeda

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var apiKey = {
+    key: '2a9640e6-745f-46a3-9365-268633625717'
+}
+
+fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=${apiKey.key}`)
+    .then((response) => {
+        if (!response.ok) throw new Error(`Erro ao executar a requisição. Status =${response.status}`);
+        return response.json();
+    })
+    .then((api) => {
+
+        for (let i = 0; i <= 10; i++) {
+            console.log(api);
+
+            var texto = "";
+
+            texto += `
+        <div clas='media'>
+            <img src='./img/coin.jpg' class='align-self-center mr-3' alt='Coin' width='100px' height='60px'>
+            <div class='media-body'>
+            <h5 class='mt-2'>Nome: ${api.data[i].name}</5>
+            <p>Simbolo: ${api.data[i].symbol}<br></p>
+            </div>
+        </div>
+        `;
+
+        document.getElementById('coins').innerHTML = texto;
+        }
+    })
+    .catch((error) => {
+        console.error(error.message);
+    });
+
